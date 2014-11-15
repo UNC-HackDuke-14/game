a69002b332db80db7d4c9694e2da1dfbac600475
Vance Miller <vbmiller@cs.unc.edu>
2014-11-15 18:22:26 -0500

    game stuff

--- javascript/game.js
+++ javascript/game.js
@@ -6,13 +6,19 @@ var stage;
 
 function init() {
     stage = new createjs.Stage("gameCanvas");
-    circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-    circle.x = circle.y = 50;
-    stage.addChild(circle);
+    paintBox(0, 0, stage);
+    paintBox(100,0,stage);
+    paintBox(0,100,stage);
+    paintBox(100,100,stage);
+}
+
+function paintBox(x, y, stage) {
+    box = new createjs.Shape();
+    box.graphics.beginFill("black").drawRect(x, y, x + 100, y + 100).beginFill("white").drawRect(x + 2, y + 2, x + 96, y + 96);
+    stage.addChild(box);
     stage.update();
 }
 
-document.ready(function () {
+$('document').ready(function () {
     init();
 });
