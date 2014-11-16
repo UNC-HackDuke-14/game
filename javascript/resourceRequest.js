08215b4f1e6023e7349311c65531993add8f4a9e
Vance Miller <vbmiller@cs.unc.edu>
2014-11-15 22:18:33 -0500

    resourceRequest skeleton

--- javascript/resourceRequest.js
+++ javascript/resourceRequest.js
@@ -1,3 +1,17 @@
 /**
  * Created by Vance Miller on 11/15/2014.
  */
+
+var resourceEnum = Object.freeze({
+    ELECTRICITY: "Electricity",
+    WATER: "Water",
+    OIL: "Oil"
+});
+
+ResourceRequest = function (type) {
+    this.type = type;
+}
+
+ResourceRequest.prototype = {
+    type: null
+}
