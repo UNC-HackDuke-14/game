d5bbeedc7b610d20fcbec62c78c630bfd95cf503
Vance Miller <vbmiller@cs.unc.edu>
2014-11-16 11:55:38 -0500

    stuff

--- javascript/game.js
+++ javascript/game.js
@@ -2,12 +2,14 @@
  * Created by Vance Miller, Sarah Rust, Andrew Park, and Kerry Ellwanger on 11/15/2014.
  */
 var game_board;
+var start_time;
 
 function init() {
     var game_stage = new createjs.Stage("gameCanvas");
     var status_stage = new createjs.Stage("statusCanvas");
     var game_state = new GameState(100, 100, 100, 100);
     var status_bars = new StatusBars(status_stage, game_state);
+    start_time = new Date().getTime();
     game_board = new GameBoard(3, 3, game_stage, game_state);
     game_board.addEventListener("click", function (e) {
         var coordinates = game_board.coordinates_to_box(e.stageX, e.stageY);
@@ -21,8 +23,10 @@ function init() {
 function loop() {
 
     createjs.Ticker.on("tick",function() {
-        if (game_board.game_state.level == 0) {
-            console.log("Game over");
+        if (game_board.game_state.level <= 0) {
+            var finish = new Date().getTime();
+            console.log("Game over. Score: " + finish - start_time);
+            createjs.Ticker.pause();
         } else {
             var rv = Math.floor(Math.random() * 100);
             if (rv < 2) {
--- javascript/resourceRequest.js
+++ javascript/resourceRequest.js
@@ -48,7 +48,7 @@ ResourceRequest.prototype = {
             var t = new Date().getTime() - this.start_time;
             var c = this.duration;
             if (c < t) {
-                this.item.game_square.game_board.game_state.level--;
+                this.item.game_square.game_board.game_state.level -= 5;
                 this.paint(false, 0);
                 e.remove();
             } else {
