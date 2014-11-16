20c198053e76d607d0f203c4dc642e6be6166c8c
Kerry Ellwanger <kerry@localhost.localdomain>
2014-11-16 01:22:19 -0500

    got sprite working

--- javascript/initSprite.js
+++ javascript/initSprite.js
@@ -1,25 +1,31 @@
 function initSprite(spriteWidth, spriteHeight, numSprites, imagepath, stage) {
-	debugger;
 	var sheet = new Image();
 	sheet.src = imagepath;
 	
 	var spriteSheet = new createjs.SpriteSheet({
 		images: [sheet],
 		frames: {
-			regX: 	spriteWidth/2,
-			regY: 	spriteHeight/2,
+			//regX: 	spriteWidth/2,
+			//regY: 	spriteHeight/2,
 			count: 	numSprites,
 			width: 	spriteWidth,
-			height: spriteHeight,
+			height: spriteHeight
 		},
 		/* {sequenceName: [firstFrame, lastFrame, NextSequenceName, Speed]}*/
 		animations: {
-			run: [0, 7, "run", 1]
-		}
+			run: [0, 25, "run", 0.5]
+		},
+	});
+
+
+	createjs.Ticker.addEventListener("tick",function(){
+		stage.update();
 	});
 	
 	/*Sprite(spriteSheet, frame/animation)*/
 	sprite = new createjs.Sprite(spriteSheet, "run");
+
+	stage.addChild(sprite);
 
 	/*Scale Sprite*/	
 	/*sprite.scaleX = spriteWidth;
