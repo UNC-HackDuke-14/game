36ee72f2b3d64bfc6048b606ff49f1b5e7162ff2
Vance Miller <vbmiller@cs.unc.edu>
2014-11-15 20:23:43 -0500

    refactored GameBoard to gameBoard.js

--- javascript/game.js
+++ javascript/game.js
@@ -20,68 +20,16 @@ function init() {
         var coords = game.coordinates_to_box(e.stageX, e.stageY);
         alert(coords[0] + "," + coords[1] + "->" + game.box_corners(coords[0], coords[1]));
     });
-
 }
 
 function paintBox(x, y, width, height, stage) {
-    box = new createjs.Shape();
+    var box = new createjs.Shape();
     box.graphics.beginFill("black").drawRect(0, 0, width, height).beginFill("white").drawRect(2, 2, width - 4, height - 4);
     box.x = x;
     box.y = y;
     stage.addChild(box);
     stage.update();
 }
-
-function GameBoard(n, m, stage) {
-    this.n = n;
-    this.m = m;
-    this.box_dim = Math.min(stage.canvas.width / n, stage.canvas.height / m);
-    this.stage = stage;
-}
-
-GameBoard.prototype = {
-    n: 0,
-    m: 0,
-    box_dim: 0,
-    stage: null,
-    coordinates_to_box: function (x, y) {
-        return [Math.floor(x / this.box_dim), Math.floor(y / this.box_dim)];
-    },
-    box_corners: function (x, y) {
-        return [x * this.box_dim, y * this.box_dim, (x + 1) * this.box_dim - 1, (y + 1 ) * this.box_dim - 1];
-    },
-    draw_boxes: function () {
-        for (var i = 0; i < this.n; i++) {
-            for (var j = 0; j < this.m; j++) {
-                var coords = this.box_corners(i, j);
-                paintBox(coords[0], coords[1], this.box_dim, this.box_dim, stage);
-            }
-        }
-    },
-    addEventListener: function (event, fn) {
-        stage.addEventListener(event, fn);
-    },
-    generateRoom: function (type) {
-        switch (type) {
-            case roomEnum.BATHROOM:
-                break;
-            case roomEnum.BEDROOM:
-                break;
-            case roomEnum.ENTERTAINMENT:
-                break;
-            case roomEnum.ENTRANCE:
-                break;
-            case roomEnum.STUDY:
-                break;
-            case roomEnum.WORKSHOP:
-                break;
-            default:
-                break;
-        }
-    }
-};
-
-
 
 $('document').ready(function () {
     init();
